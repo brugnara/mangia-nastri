23cec07e92f5565741f1941cf7624fe869b6d401
Daniele <vrut@vrut-book.local>
2024-11-22 10:52:21 +0100

    prova

--- main.go
+++ main.go
@@ -12,6 +12,13 @@ type proxyHandler struct {
 	n  int
 }
 
+func (h *proxyHandler) computeRequestHash(r *http.Request) string {
+	fmt.Println(r.Header)
+	fmt.Println(r.Method)
+
+	return "ciao lorenzino"
+}
+
 func (h *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
@@ -19,6 +26,8 @@ func (h *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "count is %d\n", h.n)
 
 	fmt.Fprintf(w, r.RequestURI)
+
+	fmt.Println(h.computeRequestHash(r))
 }
 
 func main() {
